26ba062bcea16291046a5735dd5301f5b6a788cc
Freddie Molina <freddiejmo@gmail.com>
2021-04-22 11:05:25 -0400

    added loading to portfolio home page

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -17,7 +17,7 @@ export class ProductsService {
   private loadProducts() {
     this.http.get('https://angular-html-54507-default-rtdb.firebaseio.com/products_idx.json')
       .subscribe((resp: any) => {
-        console.log('products: ', resp);
+        // console.log('products: ', resp);
         this.products = resp;
         this.loading = false;
       });
