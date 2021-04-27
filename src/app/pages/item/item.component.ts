a2aa6f5ab4fee3c33eafe32e9264772045a51996
Freddie Molina <freddiejmo@gmail.com>
2021-04-26 23:08:41 -0400

    update product page

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ProductDescription } from 'src/app/interfaces/product-description.interface';
 import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
@@ -9,13 +10,20 @@ import { ProductsService } from 'src/app/services/products.service';
 })
 export class ItemComponent implements OnInit {
 
+  // product: ProductDescription;
+  product: any;
+  id: any;
+
   constructor( private route: ActivatedRoute, public ProductsService: ProductsService) { }
 
   ngOnInit() {
     this.route.params
       .subscribe( params => {
         this.ProductsService.getProduct(params['id'])
+          // .subscribe( (product: ProductDescription) => {
           .subscribe( product => {
+            this.id = params['id'];
+            this.product = product;
             console.log('product: ', product);
           });
       });
